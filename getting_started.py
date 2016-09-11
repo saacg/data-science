a5aac9d5d6e859e4715f74016ae2959f84bd3624
saacg <isaacg@bu.edu>
2016-09-11 18:08:08 -0400

    adding first python script

---
+++ getting_started.py
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+#import libraries
+import numpy as np
+import matplotlib as mp
+import pandas as pd
+import matplotlib.pyplot as plt
+from importlib import reload
+from datetime import datetime
+from IPython.display import Image
+from IPython.display import display_html
+from IPython.display import display
+from IPython.display import Math
+from IPython.display import Latex
+from IPython.display import HTML
+print('')
+
+
+
+
+print("hi!")
+
+
+
+
+import os
+os.listdir
+
